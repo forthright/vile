056758e9c9fce9fe2a41b4274445430b83794f2d
Brent Lintner <brent.lintner@gmail.com>
2016-11-30 20:31:27 -0500

    We should add the escomplex plugin to init map.

--- src/cli/init/map.ts
+++ src/cli/init/map.ts
@@ -39,7 +39,7 @@ const PLUGIN_MAP : vile.Lib.PluginMap = {
     scss: [ "scss-lint" ],
     slim: [ "slim-lint" ],
     typescript: [ "tslint" ],
-    javascript: [ "eslint" ],
+    javascript: [ "eslint", "escomplex" ],
     php: [ "phpmd" ],
     coffeescript: [ "coffeelint" ]
   }
